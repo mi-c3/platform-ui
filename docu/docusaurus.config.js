cc2cf394573a23a02b9c1043287eb851b9227141
Denys <kitaec@list.ru>
2019-09-20 14:43:50 +0200

    fix codestyles

--- docu/docusaurus.config.js
+++ docu/docusaurus.config.js
@@ -1,86 +1,84 @@
-const path = require('path');
-
 module.exports = {
-  title: 'Platform UI',
-  tagline: 'Make your UI affectli. Build with quintessence of material-ui and our vision.',
-  url: 'https://github.com/mi-c3/platform-ui',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'mic3', // Usually your GitHub org/user name.
-  projectName: 'platform-ui-doc', // Usually your repo name.
-  themeConfig: {
-    prismTheme: require('prism-react-renderer/themes/vsDark'),
-    navbar: {
-      title: 'Platform UI Doc',
-      logo: {
-        alt: 'Platform UI Doc',
-        src: '/img/logo.192x192.png',
-      },
-      links: [
-        {to: 'docs/get-started/installation', label: 'Docs', position: 'left'},
-        {to: 'docs/docusaurus/style-guide', label: 'Markdown', position: 'left'},
-        {
-          href: 'https://github.com/mi-c3/platform-ui',
-          label: 'GitHub',
-          position: 'right',
+    title: 'Platform UI',
+    tagline: 'Make your UI affectli. Build with quintessence of material-ui and our vision.',
+    url: 'https://github.com/mi-c3/platform-ui',
+    baseUrl: '/',
+    favicon: 'img/favicon.ico',
+    organizationName: 'mic3', // Usually your GitHub org/user name.
+    projectName: 'platform-ui', // Usually your repo name.
+    themeConfig: {
+        prismTheme: require('prism-react-renderer/themes/vsDark'),
+        navbar: {
+            title: 'Platform UI Doc',
+            logo: {
+                alt: 'Platform UI Doc',
+                src: '/img/logo.192x192.png',
+            },
+            links: [
+                { to: 'docs/get-started/installation', label: 'Docs', position: 'left' },
+                { to: 'docs/docusaurus/style-guide', label: 'Markdown', position: 'left' },
+                {
+                    href: 'https://github.com/mi-c3/platform-ui',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+                {
+                    to: '/storybook/index.html',
+                    label: 'Storybook',
+                    position: 'right',
+                },
+            ],
         },
-        {
-          to: '/storybook/index.html',
-          label: 'Storybook',
-          position: 'right',
+        footer: {
+            style: 'dark',
+            // links: [
+            //   {
+            //     title: 'Docs',
+            //     items: [
+            //       {
+            //         label: 'Docs',
+            //         to: 'docs/doc1',
+            //       },
+            //     ],
+            //   },
+            //   {
+            //     title: 'Community',
+            //     items: [
+            //       {
+            //         label: 'Discord',
+            //         href: 'https://discordapp.com/invite/docusaurus',
+            //       },
+            //     ],
+            //   },
+            //   {
+            //     title: 'Social',
+            //     items: [
+            //       {
+            //         label: 'Blog',
+            //         to: 'blog',
+            //       },
+            //     ],
+            //   },
+            // ],
+            logo: {
+                alt: 'Affectli',
+                src: '/img/badge.png',
+            },
+            copyright: `Copyright © ${new Date().getFullYear()} MI-C3, Inc. Platform UI. Built with Docusaurus.`,
         },
-      ],
     },
-    footer: {
-      style: 'dark',
-      // links: [
-      //   {
-      //     title: 'Docs',
-      //     items: [
-      //       {
-      //         label: 'Docs',
-      //         to: 'docs/doc1',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: 'Community',
-      //     items: [
-      //       {
-      //         label: 'Discord',
-      //         href: 'https://discordapp.com/invite/docusaurus',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: 'Social',
-      //     items: [
-      //       {
-      //         label: 'Blog',
-      //         to: 'blog',
-      //       },
-      //     ],
-      //   },
-      // ],
-      logo: {
-        alt: 'Affectli',
-        src: '/img/badge.png',
-      },
-      copyright: `Copyright © ${new Date().getFullYear()} MI-C3, Inc. Platform UI. Built with Docusaurus.`,
-    },
-  },
-  themes: ['@docusaurus/theme-live-codeblock'],
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
+    themes: ['@docusaurus/theme-live-codeblock'],
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            },
+        ],
     ],
-  ],
 };
--- docu/src/pages/redirect.js
+++ docu/src/pages/redirect.js
@@ -2,7 +2,13 @@ import React from 'react';
 import Layout from '@theme/Layout';
 
 function Redirect() {
-    window.location.replace(`${window.location.origin}${window.location.search.substring(1)}`);
+    try {
+        if (window) {
+            window.location.replace(`${window.location.origin}${window.location.search.substring(1)}`);
+        }
+    } catch {
+        console.log('Window still not defined'); // eslint-disable-line no-console
+    }
     return <Layout title={`Redirect to storybook`}>Redirecting...</Layout>;
 }
 
