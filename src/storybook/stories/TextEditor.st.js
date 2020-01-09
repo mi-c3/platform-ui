5fe8f20e0be83877c6c60d7116f2704b6a6ce1e4
Denys Bogdanov <kitaec@list.ru>
2020-01-09 11:01:42 +0200

    docs/add-text-editor-to-docu (#78)

--- src/storybook/stories/TextEditor.st.js
+++ src/storybook/stories/TextEditor.st.js
@@ -5,10 +5,41 @@ import { action } from '@storybook/addon-actions';
 
 import TextEditor from 'components/TextEditor';
 
+const exampleValue = `
+# h1
+## h2
+### h4
+#### h5
+**bold**
+*italic*
+~~crossed~~
+[MI-C3](http://mi-c3.com/)
+> qoute
+
+some \`highlight\` word
+
+\`\`\`
+function getHelloWorld() {
+  return 'Hello World';
+}
+\`\`\`
+
+- line one
+- line two
+
+1. one
+2. two
+
+- [] checked N1
+- [] checked N2
+
+![](https://mi-c3.com/wp-content/uploads/2018/04/Earthday-201810-1-600x400.jpg)
+
+`;
 storiesOf('Components|Editors', module)
     .addDecorator(withKnobs)
     .add('TextEditor', () => (
         <Fragment>
-            <TextEditor onChange={action('onChange')} />
+            <TextEditor onChange={action('onChange')} value={exampleValue} />
         </Fragment>
     ));
