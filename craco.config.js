fdc8a81ea3cdd655f893ab32e2b184297923588c
Denys <kitaec@list.ru>
2020-02-28 11:58:18 +0200

    add react-scrits and clean the code

---
+++ craco.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    babel: {
+        presets: ['react-app'],
+        plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
+    },
+};
--- src/storybook/utils/action/action.js
+++
@@ -1,5 +0,0 @@
-import { action } from '@storybook/addon-actions';
-
-export const onChange = ({ target }) => action('onchange')({ target });
-
-export const onClick = ({ target }) => action('onclick')({ target });
