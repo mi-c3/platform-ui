e1d57b64f4e3fc2b3b288e1ee07690500430763e
Luca Pinelli <pinelli.luca@gmail.com>
2020-03-09 09:42:48 +0100

    fix/build-external-dependencies

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,8 +1,7 @@
 const eslintMiddleware = require('./.eslintrc.middleware.js');
 const jest = require('@neutrinojs/jest');
 const babelMerge = require('babel-merge');
-const standardjs = require('@neutrinojs/standardjs');
-const reactComponents = require('@neutrinojs/react-components');
+const reactLib = require('./config/reactLib');
 
 module.exports = {
   options: {
@@ -10,7 +9,16 @@ module.exports = {
   },
   use: [
     eslintMiddleware(),
-    reactComponents(),
+    reactLib({
+      externals: {
+          '@material-ui/core': 'commonjs2 @material-ui/core',
+          '@material-ui/pickers': 'commonjs2 @material-ui/pickers',
+          '@material-ui/styles': 'commonjs2 @material-ui/styles',
+          react: 'commonjs2 react',
+          'react-dom': 'commonjs2 react-dom',
+          'react-router-dom': 'commonjs2 react-router-dom',
+      }
+    }),
     neutrino => {
       neutrino.config.module
         .rule('compile')
@@ -22,24 +30,11 @@ module.exports = {
                 [ require.resolve('@babel/plugin-proposal-decorators'), { legacy: true } ],
                 require.resolve('@babel/plugin-proposal-class-properties'),
                 [ require.resolve('@babel/plugin-transform-runtime'), { regenerator: true } ]
-                // [require.resolve('module-resolver'), {
-                //     "root": ["./src"],
-                //     "alias": {
-                //       "components": "./src/components",
-                //       "styles": "./src/styles",
-                //       "utils": "./src/utils"
-                //     }
-                // }]
-
               ],
             },
             options,
           ),
         )
-      // neutrino.config.resolve
-      //   .modules
-      //     .add(neutrino.options.source);
-
     },
     jest(),
   ],
