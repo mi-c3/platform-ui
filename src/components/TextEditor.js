e5524d0556cec1280f591d3a1441f073e8d5ebb2
Luca Pinelli <pinelli.luca@gmail.com>
2020-04-17 16:12:26 +0200

    fix/#9919-disable-TextEditor

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import styled from 'styled-components';
 import ReactMde from 'react-mde';
 import * as Showdown from 'showdown';
 
@@ -15,6 +16,21 @@ if (Showdown.Converter) {
         tasklists: true,
     });
 }
+
+const Editor = styled(ReactMde)`
+    ${({ disabled }) =>
+        // eslint-disable-next-line prettier/prettier
+        disabled
+            ? `& .mde-header { display: none; }
+               & .mde-preview { min-height: 32px !important; }`
+            : ''}
+    ${({ selectedTab }) =>
+        // eslint-disable-next-line prettier/prettier
+        selectedTab === 'write'
+            ? `& .mde-header .mde-tabs button:first-child { display: none; }`
+            : `& .mde-header .mde-tabs button:last-child { display: none; }
+               & .mde-header ul { display: none; }`}
+`;
 
 class TextEditor extends PureComponent {
     static propTypes = {
@@ -64,9 +80,11 @@ class TextEditor extends PureComponent {
 
     render() {
         const { selectedTab, value } = this.state;
+        const { disabled } = this.props;
+        const state = disabled ? 'preview' : selectedTab;
         return (
-            <ReactMde
-                selectedTab={selectedTab}
+            <Editor
+                selectedTab={state}
                 onTabChange={this.setSelectedTab}
                 generateMarkdownPreview={this.buildMarkDown}
                 {...this.props}
