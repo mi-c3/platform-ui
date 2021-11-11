31a9307e5cba2f9524365212a438102d4716e2f8
Denys Bogdanov <denys.bogdanov@mi-c3.com>
2021-11-11 13:49:32 +0000

    Feature/#a728ed70 color picker randomized

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,8 +1,9 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { SwatchesPicker } from 'react-color';
+import styled from 'styled-components';
 
-import { Paper, FormControl, InputLabel, Input, InputAdornment } from '@material-ui/core';
+import { IconButton, Paper, FormControl, InputLabel, Input, InputAdornment } from '@material-ui/core';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Icon from '@material-ui/core/Icon';
 import { materialColorPalette } from 'styles/palettes';
@@ -10,6 +11,8 @@ import { withStyles } from '@material-ui/core/styles';
 import { createEvent } from 'utils/http/event';
 import { bind } from 'utils/decorators/decoratorUtils';
 import { DarkTheme } from 'styles/theme';
+
+import MdiIcon from 'components/MdiIcon';
 
 const styles = () => ({
     root: {
@@ -22,12 +25,14 @@ const styles = () => ({
             left: '8px',
             cursor: 'pointer',
         },
+        '& .MuiFormControl-root': {
+            width: '100%',
+        },
     },
     label: {
         marginLeft: 38,
     },
     swatches: {
-        width: '243px !important',
         position: 'absolute',
         zIndex: 1,
         '& > div div:nth-child(2)': {
@@ -42,12 +47,18 @@ const styles = () => ({
         borderRadius: '5rem',
     },
     wrapper: {
-        width: '100%',
         marginTop: 16,
         marginBottom: 8,
     },
     borderWrapper: { position: 'relative' },
 });
+
+const SwatchesPickerStyled = styled(SwatchesPicker)`
+    width: ${({ width }) => `${width}px`};
+`;
+const IconButtonStyled = styled(IconButton)`
+    margin-top: -16px;
+`;
 
 class ColorPicker extends PureComponent {
     static propTypes = {
@@ -55,8 +66,10 @@ class ColorPicker extends PureComponent {
         name: PropTypes.string,
         label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
         value: PropTypes.string,
+        randomized: PropTypes.bool,
         required: PropTypes.bool,
         disabled: PropTypes.bool,
+        clearable: PropTypes.bool,
         fontSize: PropTypes.string,
         labelPlacement: PropTypes.string,
         className: PropTypes.string,
@@ -66,28 +79,53 @@ class ColorPicker extends PureComponent {
     static defaultProps = {
         disabled: false,
         required: false,
+        clearable: false,
     };
 
     constructor(props) {
         super(props);
 
-        this.state = { displayColorPicker: false };
-        if (!props.value) {
+        this.state = { displayColorPicker: false, wrapperWidth: '100%' };
+
+        if (!props.value && !props.randomized) {
             this.onChange({ hex: this.defaultValue });
         }
+        if (!props.value && props.randomized) {
+            this.onChange({ hex: this.getRandomColor() });
+        }
     }
-
+    wrapperRef = React.createRef();
     defaultValue = '#00BCD4';
     defaultColors = [
         ['#066ab1', '#0779ca'],
         ['#FFFFFF', '#00a99d', '#066ab1', '#4FC3F7', '#81C784', '#FF8A65', '#FFC107', '#FF5722', '#c62828'],
     ];
 
+    componentDidMount() {
+        this.setState({ wrapperWidth: this.wrapperRef.current.scrollWidth });
+        window.addEventListener('resize', () => {
+            if (this.state.wrapperWidth !== this.wrapperRef.current.scrollWidth) {
+                this.setState({ wrapperWidth: this.wrapperRef.current.scrollWidth });
+            }
+        });
+    }
+
+    @bind
+    getRandomColor() {
+        const randomGroup = Math.floor(Math.random() * materialColorPalette?.length || 0);
+        const randomColor = Math.floor(Math.random() * materialColorPalette[randomGroup]?.length || 0);
+        return materialColorPalette[randomGroup][randomColor];
+    }
+
     @bind
     onChange(value) {
-        const { onChange, name } = this.props;
+        const { onChange, name, randomized } = this.props;
         if (onChange) {
-            onChange(createEvent('change', { target: { name, value: (value || {}).hex } }));
+            let nextValue = value?.hex;
+            if (!nextValue && randomized) {
+                nextValue = this.getRandomColor();
+            }
+            onChange(createEvent('change', { target: { name, value: nextValue } }));
         }
     }
 
@@ -96,11 +134,22 @@ class ColorPicker extends PureComponent {
         this.setState((state) => ({ displayColorPicker: !state.displayColorPicker }));
     }
 
+    @bind
+    buildEndAdornment(value, clearable, disabled) {
+        if (value && clearable && !disabled) {
+            return (
+                <IconButtonStyled aria-label="Clear input" onClick={() => this.onChange(null)}>
+                    <MdiIcon name="close" />
+                </IconButtonStyled>
+            );
+        }
+    }
+
     render() {
-        const { label, name, value, required, fontSize, classes, disabled, className, ...restProps } = this.props;
-        const { displayColorPicker } = this.state;
+        const { label, name, value, required, clearable, fontSize, classes, disabled, className, ...restProps } = this.props;
+        const { displayColorPicker, wrapperWidth } = this.state;
         return (
-            <div className={`${className || ''} ${classes.wrapper} ColorPicker-wrapper`}>
+            <div className={`${className || ''} ${classes.wrapper} ColorPicker-wrapper`} ref={this.wrapperRef}>
                 <Paper component="form" className={classes.root}>
                     <FormControl required={required} onClick={this.handleSwatches} disabled={disabled}>
                         <InputLabel className={classes.label}>{label}</InputLabel>
@@ -108,26 +157,23 @@ class ColorPicker extends PureComponent {
                             id="input-with-icon-adornment"
                             startAdornment={
                                 <InputAdornment position="start">
-                                    <Icon
-                                        fontSize={fontSize}
-                                        style={{ backgroundColor: value || this.defaultValue }}
-                                        name={'circle'}
-                                        className={classes.icon}
-                                    />
+                                    <Icon fontSize={fontSize} style={{ backgroundColor: value }} name={'circle'} className={classes.icon} />
                                 </InputAdornment>
                             }
-                            value={value || this.defaultValue}
+                            endAdornment={this.buildEndAdornment(value, clearable, disabled)}
+                            value={value || ''}
                             disableUnderline
                         />
                     </FormControl>
                 </Paper>
                 {!disabled && displayColorPicker && (
-                    <div className={classes.borderWrapper}>
+                    <div className={classes.borderWrapper} style={{ width: wrapperWidth }}>
                         <ClickAwayListener onClickAway={this.handleSwatches}>
-                            <SwatchesPicker
+                            <SwatchesPickerStyled
                                 {...restProps}
+                                width={wrapperWidth}
                                 name={name}
-                                color={value || this.defaultValue}
+                                color={value}
                                 colors={materialColorPalette || this.defaultColors}
                                 onChange={this.onChange}
                                 className={classes.swatches}
