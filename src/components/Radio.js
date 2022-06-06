53d6f1e4fd94ca1f9e8f0486fad17f6f7b5a0048
denys.bogdanov <denysboganov@mi-c3.com>
2022-06-06 10:06:35 +0300

    fix/#72b4ee6e-fix-radio-component

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import MuiRadio from '@material-ui/core/Radio';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { createEvent } from 'utils/http/event';
+import { bind } from 'utils/decorators/decoratorUtils';
 
-const Radio = ({ labelPlacement, color, RadioProps, ...restProps }) => (
-    <FormControlLabel {...restProps} control={<MuiRadio color={color} {...RadioProps} />} labelPlacement={labelPlacement || 'end'} />
-);
+class Radio extends PureComponent {
+    static propTypes = {
+        ...(MuiRadio || {}).propTypes,
+        label: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+        labelPlacement: PropTypes.string,
+    };
 
-Radio.propTypes = {
-    ...(MuiRadio || {}).propTypes,
-    color: PropTypes.string,
-    labelPlacement: PropTypes.string,
-};
+    static defaultProps = {
+        color: 'primary',
+    };
 
-Radio.defaultProps = {
-    color: 'primary',
-};
+    @bind
+    onChange(event) {
+        const { name, onChange } = this.props;
+        const { checked } = event.target;
+        onChange &&
+            onChange(
+                createEvent('change', {
+                    target: { name, checked, value: checked },
+                    originalEvent: event,
+                }),
+                event
+            );
+    }
+
+    render() {
+        const { RadioProps, labelPlacement, color, value, ...restProps } = this.props;
+        return (
+            <FormControlLabel
+                {...restProps}
+                checked={value || false}
+                onChange={this.onChange}
+                control={<MuiRadio color={color} {...RadioProps} />}
+                labelPlacement={labelPlacement || 'end'}
+            />
+        );
+    }
+}
 
 export default Radio;
